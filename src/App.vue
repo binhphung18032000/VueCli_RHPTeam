1f35cada8e86381bc2bd9345526b2604d1b5d36d
binhphung <binhphung18032000@gmail.com>
2022-05-06 09:02:32 +0700

    updated 04/05/2022

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <!-- <h1 class="fade">Hello anh em. Welcome to Vue CLI</h1> -->
+  <!-- <base-modal
+    v-if="isShowModal"
+    title="This is the title called from App.vue"
+    content="This is the content called from App.vue"
+    theme="sales"
+    @cancel="onToggleModal"
+  /> -->
+  <!-- <teleport to="body">
+    <base-modal
+      v-if="isShowModal"
+      title="This is the title called from App.vue"
+      content="This is the content called from App.vue"
+      theme="sales"
+      @cancel="onToggleModal"
+    >
+      <template v-slot:header>
+        <h2>This is a header</h2>
+      </template>
+      <label for="">Email</label>
+      <input type="email" />
+      <template v-slot:footer>
+        <button @click="onToggleModal">Cancel</button>
+      </template>
+    </base-modal>
+  </teleport>
+  <button @click="onToggleModal">Toggle modal</button> -->
+  <quiz-form />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+// import MyModal from "./components/MyModal.vue";
+import QuizForm from "./components/QuizForm.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    // BaseModal: MyModal,
+    QuizForm,
+  },
+  data() {
+    return {
+      isShowModal: false,
+    };
+  },
+  methods: {
+    onToggleModal() {
+      console.log("RUN RUN RUN");
+      this.isShowModal = !this.isShowModal;
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyModal.vue
@@ -0,0 +1,141 @@
+<template>
+  <!-- @click="onCloseModal" -->
+  <div class="modal fade show" @click.self="onCloseModal">
+    <div class="modal-dialog">
+      <div class="modal-content" :class="{ 'bg-blue': theme === 'sales' }">
+        <div class="modal-header">
+          <!-- Header -->
+          <!-- {{ heading }} -->
+          <!-- <h1>{{ title }}</h1> -->
+          <slot name="header"></slot>
+        </div>
+        <div class="modal-body">
+          <!-- Body -->
+          <!-- <p>{{ content }}</p> -->
+          <slot />
+        </div>
+        <div class="modal-footer">
+          <!-- Footer -->
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    theme: {
+      type: String,
+      default: "example",
+      validator(value) {
+        return ["example", "sales", "contact"].includes(value);
+      },
+    },
+  },
+  data() {
+    return {
+      // heading: "This is the title",
+    };
+  },
+  methods: {
+    onCloseModal() {
+      this.$emit("cancel");
+    },
+  },
+};
+</script>
+
+<style lang="css">
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1060;
+  /* display: none; */
+  display: block;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
+  /* background-color: rgba(255, 4, 4, 0.2); */
+}
+
+.fade {
+  transition: opacity 0.15s linear;
+}
+
+.modal.fade .modal-dialog {
+  transition: transform 0.3s ease-out;
+  transform: translate(0, -50px);
+}
+
+.modal-dialog {
+  max-width: 500px;
+  margin: 4.75rem auto;
+}
+
+.modal-dialog {
+  position: relative;
+  width: auto;
+  /* margin: 0.5rem; */
+  pointer-events: none;
+}
+
+.modal-content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  pointer-events: auto;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.3rem;
+  outline: 0;
+  color: #fff;
+}
+
+.modal-content.bg-blue {
+  background-color: blue;
+}
+
+.modal-header {
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 1rem;
+  border-bottom: 1px solid #dee2e6;
+  border-top-left-radius: calc(0.3rem - 1px);
+  border-top-right-radius: calc(0.3rem - 1px);
+}
+
+.modal-body {
+  position: relative;
+  flex: 1 1 auto;
+  padding: 1rem;
+}
+
+.modal-footer {
+  display: flex;
+  flex-wrap: wrap;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0.75rem;
+  border-top: 1px solid #dee2e6;
+  border-bottom-right-radius: calc(0.3rem - 1px);
+  border-bottom-left-radius: calc(0.3rem - 1px);
+}
+</style>
